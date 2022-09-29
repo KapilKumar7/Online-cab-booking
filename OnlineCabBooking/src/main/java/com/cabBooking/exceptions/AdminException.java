328cdd6e13879ab4309bac31fe60a2609b03a407
asheesh jaat <asheeshjaat2175@gmail.com>
2022-09-29 10:01:54 +0530

    day 2 exceptions

---
+++ OnlineCabBooking/src/main/java/com/cabBooking/exceptions/AdminException.java
@@ -0,0 +1,13 @@
+package com.cabBooking.exceptions;
+
+public class AdminException   extends Exception{
+
+	
+	public AdminException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public AdminException(String message) {
+		super(message);
+	}
+}
